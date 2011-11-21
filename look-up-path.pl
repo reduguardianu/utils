630fe57d19eb8d7af20ecee06ed4c8ce8a35f63d
Marek Ozimina <marek.ozimina@cantstopgames.com>
2011-11-21 09:45:53 +0100

    Init commit

---
+++ look-up-path.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use Cwd;
+sub get_path
+{
+    do
+    {
+        my $path = getcwd();
+
+        if (-e $ARGV[0])
+        {
+            return $path;
+        }
+        chdir("../");
+    } while($path ne "/");
+
+    die "no file in uptree";
+}
+
+my $path = get_path();
+
+print $path;
